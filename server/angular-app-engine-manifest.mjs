bebbb0184223cc90134e6a4bf4e8399c504c056b
Tarun <tarun@gmail.com>
2025-02-18 13:48:18 +0530

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://vignesh31j.github.io/iPlanetClone',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
